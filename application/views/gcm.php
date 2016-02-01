3355f3a44d50428835af3506d3b6d596e1ed5489
Mohan Manu <mohan@fewgoodpeople.com>
2016-02-01 17:35:30 +0530

    first commit

---
+++ application/views/gcm.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Gcm</title>
+    </head>
+    <body>
+        
+        <form method="post">
+            <table>
+                <tr>
+                    <td>GCMKEY :</td><td><input type="text" width="200px"/></td>
+                </tr>
+                <tr>
+                    <td>Title  :</td><td><input type="text"/></td>
+                </tr>
+                 <tr>
+                    <td>Message:</td><td><input type="text"/></td>
+                </tr>
+                <tr>
+                    <td></td><td><input type="submit"/></td>
+                </tr>
+        </form>
+        
+        
+        
+    </body>
+    
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ application/views/test.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE HTML>
+<html>
+    <head><title>test</title></head>
+    <body>
+        <header align="center">
+            <h1>This is mohan local website</h1>
+        </header>
+        <article>
+            <table border=1px>
+                <col
+                <tr>
+                    <td>id</td><td>name</td><td>age</td>
+                </tr>
+            </table>
+        </article>
+        
+        
+       
+    </body>
+    
+</html>
+
+
+
