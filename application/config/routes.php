147fe4749f43c1cf4abece521ec5358f2788958f
Mohan Manu <mohan@fewgoodpeople.com>
2016-02-14 01:40:33 +0530

    changed index file

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'welcome/gcm';
 $route['gcm/(:num)']='welcome/gcm';
 $route['gcm']='welcome/gcm';
 $route['recycler']='welcome/recycler';
