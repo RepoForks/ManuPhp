c5c4c4a8db67bbff030bc668efae704c01750a4b
Mohan Manu <mohan@fewgoodpeople.com>
2016-02-14 01:28:41 +0530

    added remote db configuration

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'prod';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -79,6 +79,27 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'manu',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['prod'] = array(
+	'dsn'	=> '',
+	'hostname' => 'mysql1.000webhost.com',
+	'username' => 'a7928610_manu',
+	'password' => 'Anu08223',
+	'database' => 'a7928610_manudat',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
